ffbbfe62f9cf064d96660d6b668bf1b1481a5ec9
thetana <zxsa135@naver.com>
2026-01-09 11:03:50 +0900

    사칙연산 기호를 입력 받을 코드 구현

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -6,11 +6,15 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int n1 = sc.nextInt();
-        System.out.println(n1);
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int n2 = sc.nextInt();
-        System.out.println(n2);
+//        System.out.print("첫 번째 숫자를 입력하세요: ");
+//        int n1 = sc.nextInt();
+//        System.out.println(n1);
+//        System.out.print("두 번째 숫자를 입력하세요: ");
+//        int n2 = sc.nextInt();
+//        System.out.println(n2);
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char c = sc.next().charAt(0);
+        System.out.println(c);
     }
 }
