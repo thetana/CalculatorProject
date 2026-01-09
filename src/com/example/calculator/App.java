f5ad0bf53dcfdcca8a14ea865b337c659bf1e152
thetana <zxsa135@naver.com>
2026-01-09 10:56:08 +0900

    프로젝트 생성 첫 커밋

---
+++ src/com/example/calculator/App.java
@@ -0,0 +1,14 @@
+package com.example.calculator;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+    }
+}
