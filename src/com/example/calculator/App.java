e73ad683caaa49d6cdd38af6c2ba448775ebd97f
thetana <zxsa135@naver.com>
2026-01-09 11:20:28 +0900

    4칙 연산 계산 코드 구현 및 0나누기 예외처리

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -6,15 +6,37 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-//        System.out.print("첫 번째 숫자를 입력하세요: ");
-//        int n1 = sc.nextInt();
-//        System.out.println(n1);
-//        System.out.print("두 번째 숫자를 입력하세요: ");
-//        int n2 = sc.nextInt();
-//        System.out.println(n2);
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int n1 = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int n2 = sc.nextInt();
 
         System.out.print("사칙연산 기호를 입력하세요: ");
         char c = sc.next().charAt(0);
-        System.out.println(c);
+
+        int result = 0;
+        try {
+            switch (c) {
+                case '+':
+                    result = n1 + n2;
+                    break;
+                case '-':
+                    result = n1 - n2;
+                    break;
+                case '*':
+                    result = n1 * n2;
+                    break;
+                case '/':
+                    result = n1 / n2;
+                    break;
+                default:
+                    System.out.println("연산 할 수 없는 기호 입니다.");
+            }
+            System.out.println("결과: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("0으로 나눌 수 없습니다.");
+        } catch (Exception e) {
+            System.out.println("예상치 못한 에러 입니다.");
+        }
     }
 }
